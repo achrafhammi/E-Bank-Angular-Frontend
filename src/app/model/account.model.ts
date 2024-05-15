f5d66c36ef46e4e2fa44357a28146836e8ef6945
achrafhammi <achrafhammi29@gmail.com>
2024-05-15 01:03:33 +0100

    Fixed accounts

--- src/app/model/account.model.ts
+++ src/app/model/account.model.ts
@@ -4,13 +4,13 @@ export interface AccountDetails {
   currentPage:          number;
   totalPages:           number;
   pageSize:             number;
-  accountOperationDTOS: AccountOperation[];
+  operationsDTO: AccountOperation[];
 }
 
 export interface AccountOperation {
   id:            number;
   operationDate: Date;
   amount:        number;
-  type:          string;
+  operationType:          string;
   description:   string;
 }
